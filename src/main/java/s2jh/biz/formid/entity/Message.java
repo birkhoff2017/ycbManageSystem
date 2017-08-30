08763844306031d3cbdccc1add3aef9c8d40a647
duxinyuan <215041225@qq.com>
2017-08-30 15:14:05 +0800

    添加form_id实体类

---
+++ src/main/java/s2jh/biz/formid/entity/Message.java
@@ -0,0 +1,51 @@
+package s2jh.biz.formid.entity;
+
+import lab.s2jh.core.annotation.MetaData;
+import lab.s2jh.core.entity.BaseNativeEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+
+/**
+ * Created by duxinyuan on 17-8-28.
+ */
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@Access(AccessType.FIELD)
+@Entity
+@Table(name = "ycb_mcs_message")
+@MetaData(value = "业务模块:推送消息")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Message extends BaseNativeEntity {
+
+    private static final long serialVersionUID = -6311575125667643141L;
+
+    @MetaData("openid")
+    @Column(name = "openid")
+    private String openid;
+
+    //form_id或prepay_id
+    @MetaData("form_prepay_id")
+    @Column(name = "form_prepay_id")
+    private String form_prepay_id;
+
+    @MetaData("type") // 1为form_id, 2为prepay_id
+    @Column(name = "type")
+    private Integer type;
+
+    @MetaData("orderid")
+    @Column(name = "orderid")
+    private String orderid;
+
+    //次数  form_id只能使用一次，prepay_id可以使用三次
+    @MetaData("number")
+    @Column(name = "number")
+    private Integer number;
+
+}
